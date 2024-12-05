d29d98fbc79a5eeb011e5f34e1922424a9ab5010
Tatianajimenez12 <tatiana_jr@tesch.edu.mx>
2024-12-05 14:39:01 -0600

    Update ExampleInstrumentedTest.kt
    Este es un nuevo comentario

--- app/src/androidTest/java/com/example/ejemplogit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ejemplogit/ExampleInstrumentedTest.kt
@@ -19,6 +19,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        //Verifica que el nombre del paquete del contexto de la aplicación coincida con el valor esperado.
         assertEquals("com.example.ejemplogit", appContext.packageName)
     }//hola que tal
 }
